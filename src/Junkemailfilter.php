3c1e031683b7e7a52e082c338121d9f6d879b7c4
Bart Vrancken <bart@abuse.io>
2015-12-30 12:43:45 +0100

    add docblocks

--- src/Junkemailfilter.php
+++ src/Junkemailfilter.php
@@ -2,10 +2,17 @@
 
 namespace AbuseIO\Parsers;
 
+/**
+ * Class Junkemailfilter
+ * @package AbuseIO\Parsers
+ */
 class Junkemailfilter extends Parser
 {
     /**
      * Create a new Junkemailfilter instance
+     *
+     * @param \PhpMimeMailParser\Parser $parsedMail phpMimeParser object
+     * @param array $arfMail array with ARF detected results
      */
     public function __construct($parsedMail, $arfMail)
     {
@@ -14,7 +21,7 @@ class Junkemailfilter extends Parser
 
     /**
      * Parse attachments
-     * @return Array    Returns array with failed or success data
+     * @return array    Returns array with failed or success data
      *                  (See parser-common/src/Parser.php) for more info.
      */
     public function parse()
@@ -60,4 +67,3 @@ class Junkemailfilter extends Parser
         return $this->success();
     }
 }
-
